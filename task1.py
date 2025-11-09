96f9eb4e2e9484f07b803cad53215adcfb53a054
kaustubhsorte2012-alt <kaustubhsorte2012@gmail.com>
2025-11-10 01:59:25 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+
+
+
+x1=int(input ("Enter the first number:"))
+x2=int(input ("Enter the second number:"))
+
+print("Addition:",x1+x2)
+print("Subtraction:",x1-x2)
+print("Multiplication:",x1*x2)
+print("Division:",x1/x2)
