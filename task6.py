abffc392ee01052e242c74db587f51489406e0e1
kaustubhsorte2012-alt <kaustubhsorte2012@gmail.com>
2026-01-07 15:37:07 +0530

    Add files via upload

---
+++ task6.py
@@ -0,0 +1,13 @@
+import math
+
+
+num = float(input("Enter a number: "))
+
+square_root = math.sqrt(num)
+natural_log = math.log(num)
+sine_value = math.sin(num)
+
+
+print("Square root of", num, "is:", square_root)
+print("Natural logarithm of", num, "is:", natural_log)
+print("Sine of", num, "is:", sine_value)
